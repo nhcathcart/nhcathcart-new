331c5f79eb046d0665501a2bfbf414155bf168b9
Nicholas Cathcart <nhcathcart@gmail.com>
2024-04-23 22:10:53 -0400

    FIX: fix loader scroll bug.

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -3,7 +3,7 @@ import { Logo } from './logo';
 
 export default function Loader() {
   return (
-    <div className="pointer-events-none absolute z-[1000] flex h-[calc(100vh-80px)] min-w-full animate-fadeout items-center justify-center bg-background lg:h-[calc(100vh-80px)]">
+    <div className="pointer-events-none absolute z-[1000] flex h-full min-w-full animate-fadeout items-center justify-center bg-background lg:h-[calc(100vh-80px)]">
       <div className="h-[250px] w-[250px] md:h-[400px] md:w-[400px]">
         <Logo />
       </div>
